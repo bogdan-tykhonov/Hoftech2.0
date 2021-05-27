b4ec8ca1f761b1af88ee49a25038b451a5466a32
MacBook <macbook@MacBooks-MacBook-Pro.local>
2021-05-27 21:12:59 +0300

    update js

--- assets/js/Portfolio.js
+++ assets/js/Portfolio.js
@@ -1,57 +1,59 @@
  class Portfolio{
-    static cases = [
-        {
-            id: 1,
-            color: '#FECE2F',
-            title: 'FitSpas',
-            description: 'Сайт по продаже программы для лечения спины',
-            link: 'https://spyna.fitspas.com.ua/',
+        cases = [
+            {
+                id: 1,
+                color: '#FECE2F',
+                title: 'FitSpas',
+                description: 'Сайт по продаже программы для лечения спины',
+                link: 'https://spyna.fitspas.com.ua/',
+    
+            },
+            {
+                id: 2,
+                color: '#17a2b8',
+                title: 'Vue mini apps',
+                description: 'Одностраничное приложения с мини программами',
+                link: 'https://bogdan-tykhonov.github.io/vue-mini-apps/',
+    
+            },
+            
+            {
+                id: 3,
+                color: '#FFBC00',
+                title: 'Оздоровча гімнастика',
+                description: 'Сайт по продаже программы оздоровительной гимнастики ',
+                link: 'https://9.fitspas.com.ua/',
+    
+            },
+            {
+                id: 4,
+                color: '#f74c49;',
+                title: 'Etrans',
+                description: 'Сайт логистической компании',
+                link: 'https://www.e-trans.com.ua/',
+    
+            },
+            {
+                id: 5,
+                color: '#FFA500',
+                title: 'Komorra',
+                description: 'Сайт для продажи спортивного контент плана',
+                link: 'http://ma.komorra.com.ua/',
+    
+            },
+            {
+                id: 6,
+                color: '#9c2434',
+                title: 'Cd School',
+                description: 'Сайт школы дизайн интерьера',
+                link: 'https://cd-school.com.ua/',
+    
+            },
+        ];
+     
+     
 
-        },
-        {
-            id: 2,
-            color: '#17a2b8',
-            title: 'Vue mini apps',
-            description: 'Одностраничное приложения с мини программами',
-            link: 'https://bogdan-tykhonov.github.io/vue-mini-apps/',
-
-        },
-        
-        {
-            id: 3,
-            color: '#FFBC00',
-            title: 'Оздоровча гімнастика',
-            description: 'Сайт по продаже программы оздоровительной гимнастики ',
-            link: 'https://9.fitspas.com.ua/',
-
-        },
-        {
-            id: 4,
-            color: '#f74c49;',
-            title: 'Etrans',
-            description: 'Сайт логистической компании',
-            link: 'https://www.e-trans.com.ua/',
-
-        },
-        {
-            id: 5,
-            color: '#FFA500',
-            title: 'Komorra',
-            description: 'Сайт для продажи спортивного контент плана',
-            link: 'http://ma.komorra.com.ua/',
-
-        },
-        {
-            id: 6,
-            color: '#9c2434',
-            title: 'Cd School',
-            description: 'Сайт школы дизайн интерьера',
-            link: 'https://cd-school.com.ua/',
-
-        },
-    ];
-
-    static init(){
+     init(){
         this.cases.forEach( work => {
             const projectCard = `<div class="project-card">
             <div class="project front" >
@@ -83,7 +85,8 @@
         
     }
 };
-window.addEventListener("load", function() {
+document.addEventListener("DOMContentLoaded", function() {
  
-    Portfolio.init();
+   const portfolio = new Portfolio();
+   portfolio.init();
   });
